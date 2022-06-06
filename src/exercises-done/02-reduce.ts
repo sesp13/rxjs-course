85efb3c5b40e174fee85377c934499c4c9fd36c5
sesp13 <sespinosarte99@gmail.com>
2022-06-06 10:28:57 -0500

    add exercise module

---
+++ src/exercises-done/02-reduce.ts
@@ -0,0 +1,26 @@
+import { from, reduce } from 'rxjs';
+
+/**
+ * Ejercicio:
+ * Sume todos los números del arreglo usando un reduce.
+ * Debe de filtrar para que sólo números sean procesados
+ * La salida debe de ser 32
+ *
+ * Tip:
+ * isNan() es una función de JavaScript para determinar si es número
+ * Usar filter<any>(...) para no tener problemas de tipado.
+ */
+
+(() => {
+  const datos = [1, 2, 'foo', 3, 5, 6, 'bar', 7, 8];
+
+  from(datos)
+    .pipe(
+      // Trabajar aquí
+      reduce((accumulator, next) => {
+        const nextNumber = typeof next != 'number' ? 0 : next;
+        return accumulator + nextNumber;
+      }, 0)
+    )
+    .subscribe(console.log); // La salida debe de ser 32
+})();
